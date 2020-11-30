aca98299c6cefbd481f0b3fa4482c1c82b16e8a1
Amer Klinic <aklinic1@etf.unsa.ba>
2020-11-30 17:37:55 +0100

    tut 6 priprema

--- src/ba/unsa/etf/rpr/tutorijal06/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal06/Main.java
@@ -6,13 +6,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.nio.file.attribute.UserDefinedFileAttributeView;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/digitron.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("digitron.fxml"));
         primaryStage.setTitle("Digitron");
-        primaryStage.setScene(new Scene(root, 400, 400));
+        primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.setMinHeight(204);
+        primaryStage.setMinWidth(132);
         primaryStage.show();
     }
 
