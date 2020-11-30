3c40c5c60811382b624345d91558595a4c7af065
Amer Klinic <aklinic1@etf.unsa.ba>
2020-11-30 19:28:08 +0100

    kraj tuta commit

--- src/ba/unsa/etf/rpr/tutorijal06/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal06/Controller.java
@@ -1,5 +1,24 @@
 package ba.unsa.etf.rpr.tutorijal06;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
+
+import java.awt.*;
+
+public class Controller {
+    public TextField txtField;
+
+    public void bttnClick(ActionEvent actionEvent) {
+        Button button = (Button)actionEvent.getSource();
+        txtField.setText(txtField.getText() + button.getText());
+    }
+
+    public void bttnEqualsClick(ActionEvent actionEvent) {
+
+    }
+
+    public void bttnActionClick(ActionEvent actionEvent) {
+    }
 }
