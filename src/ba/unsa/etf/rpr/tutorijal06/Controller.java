abc76e8413bc3d4390917557bb4206ff7326630c
Amer Klinic <aklinic1@etf.unsa.ba>
2020-12-01 17:45:32 +0100

    dodane sve funkcionalnosti

--- src/ba/unsa/etf/rpr/tutorijal06/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal06/Controller.java
@@ -6,19 +6,44 @@ import javafx.scene.control.TextField;
 
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Controller {
     public TextField txtField;
+    private Double broj1, broj2;
+    private String operacija = new String("");
+
+    private String dajRezultat(){
+        Double rezultat;
+        if(operacija.equals("+")) rezultat = broj1 + broj2;
+        else if(operacija.equals("-")) rezultat = broj1 - broj2;
+        else if(operacija.equals("/")) rezultat = broj1 / broj2;
+        else if(operacija.equals("x")) rezultat = broj1 * broj2;
+        else rezultat = broj1 % broj2;
+        return String.valueOf(rezultat);
+    }
 
     public void bttnClick(ActionEvent actionEvent) {
+        if(txtField.getText().equals("0")) txtField.setText("");
         Button button = (Button)actionEvent.getSource();
         txtField.setText(txtField.getText() + button.getText());
     }
 
     public void bttnEqualsClick(ActionEvent actionEvent) {
-
+        broj2 = Double.valueOf(txtField.getText());
+        txtField.setText(dajRezultat());
+        obnoviDigitron();
+    }
+    private void obnoviDigitron(){
+        broj1 = Double.valueOf(txtField.getText());
     }
 
-    public void bttnActionClick(ActionEvent actionEvent) {
+    public void bttnOperationClick(ActionEvent actionEvent) {
+        Button button = (Button)actionEvent.getSource();
+        broj1 = Double.valueOf(txtField.getText());
+        operacija = button.getText();
+        txtField.setText("0");
     }
 }
